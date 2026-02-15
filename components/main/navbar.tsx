56cfe1fb56b2b0c1c5e2f20d529ad28b7f1e3fa6
Lokpavan P <141185596+pavan1832@users.noreply.github.com>
2026-02-15 18:06:37 +0530

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -41,15 +41,7 @@ export const Navbar = () => {
               </Link>
             ))}
 
-            {/* Source Code */}
-            <Link
-              href={LINKS.sourceCode}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
-            >
-              Source Code
-            </Link>
+           
           </div>
         </div>
 
